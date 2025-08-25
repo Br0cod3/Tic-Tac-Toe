38d22ace7b10e24a589468e8387de21157d473dc
Br0cod3 <thespammingbott@gmail.com>
2025-08-25 15:00:48 +0100

    implement: getBoard, placemark and resetBoard functions

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
-const Gameboard = (function() {
+const Gameboard = (function () {
   const row = 3;
   const columns = 3;
-  const board = []
+  const board = [];
 
   for (let i = 0; i < row; i++) {
-    board[i] = []
+    board[i] = [];
     for (let k = 0; k < columns; k++) {
-      board[i][k] = null
+      board[i][k] = null;
     }
   }
 
-  console.log(board)
+  const getBoard = () => board;
+
+  const placemark = (row, column, char) => {
+    board[row][column] = char;
+  };
+
+  const resetBoard = () => {
+    for (let i = 0; i < row; i++) {
+      board[i] = [];
+      for (let k = 0; k < columns; k++) {
+        board[i][k] = null;
+      }
+    }
+  };
+
+  return { getBoard, placemark, resetBoard }
 })();
