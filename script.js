a599bf0619300512a19c6359b236b7f5bdbbd2a1
Br0cod3 <thespammingbott@gmail.com>
2025-08-25 14:27:53 +0100

    refactored initial code, new code now follows IIFE module pattern

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
-function Gameboard() {
-    const rows = 3
-    const columns = 3
-    const board = []
+const Gameboard = (function() {
+  const row = 3;
+  const columns = 3;
+  const board = []
 
-    for(let i =  0; i < rows; i++) {
-        board[i] = []
-        for (let k = 0; k < columns; k++) {
-            board[i][k] = [null]
-        }
+  for (let i = 0; i < row; i++) {
+    board[i] = []
+    for (let k = 0; k < columns; k++) {
+      board[i][k] = null
     }
-    return {board};
-}
+  }
 
-const board = Gameboard()
-console.log(board.board)
-console.log(board)
+  console.log(board)
+})();
