c51c76ab3625e9a4c88948dc15d106eb8ebbe8db
Br0cod3 <thespammingbott@gmail.com>
2025-08-27 05:19:40 +0100

    implement: GameBoard to generate, place a mark and reest the board

--- script.js
+++ script.js
@@ -72,3 +72,40 @@ const GamePilot = (function () {
 
   return { generatePlayer, checkWin };
 })();
+
+
+const Gameboard = (function () {
+  const row = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < row; i++) {
+    board[i] = [];
+    for (let k = 0; k < columns; k++) {
+      board[i][k] = null;
+    }
+  }
+
+  const getBoard = () => board;
+
+  const placemark = function(row, column, char, player) {
+    if (board[row][column] !== null) {
+      console.log("unable to move, that space is taken");
+      return false;
+    }
+    board[row][column] = char;
+    player.myTurn = false;
+    return true;
+  };
+
+  const resetBoard = () => {
+    for (let i = 0; i < row; i++) {
+      board[i] = [];
+      for (let k = 0; k < columns; k++) {
+        board[i][k] = null;
+      }
+    }
+  };
+
+  return { getBoard, placemark, resetBoard };
+})();
