921d1416cc84da43c11b29ada867b85aa9ce33c5
Br0cod3 <thespammingbott@gmail.com>
2025-08-27 05:08:37 +0100

    implement: GamePilot function to generate players

--- script.js
+++ script.js
@@ -1,52 +1,13 @@
-const Gameboard = (function () {
-  const row = 3;
-  const columns = 3;
-  const board = [];
-
-  for (let i = 0; i < row; i++) {
-    board[i] = [];
-    for (let k = 0; k < columns; k++) {
-      board[i][k] = null;
-    }
-  }
-
-  const getBoard = () => board;
-
-  const placemark = (row, column, char) => {
-    board[row][column] = char;
+const GamePilot = (function () {
+  const generatePlayer = function (name, value) {
+    return {
+      name,
+      value,
+      myTurn: false,
+      makeMove: function(row, column) {
+        if (this.myTurn) {
+          Gameboard.placemark(row, column, this.value, this)
+        }
+      },
+    };
   };
-
-  const resetBoard = () => {
-    for (let i = 0; i < row; i++) {
-      board[i] = [];
-      for (let k = 0; k < columns; k++) {
-        board[i][k] = null;
-      }
-    }
-  };
-
-  return { getBoard, placemark, resetBoard }
-})();
-
-const GameController = (function() {
-  let player = 0
-  let char;
-  
-  const makePlayer = (name) => {
-    if (player > 1) return;
-    
-    char = player === 0 ? "X" : "O"
-    playerIncrement()
-    return { name, char}
-  }
-
-  const playerIncrement = () => player++;
-
-  return { makePlayer }
-
-})()
-
-console.log(GameController.makePlayer("Mike"))
-console.log(GameController.makePlayer("Mary"))
-console.log(GameController.makePlayer("Mark"))
-console.log(GameController.makePlayer("Mason"))
